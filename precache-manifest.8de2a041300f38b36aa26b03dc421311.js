5b23fc1e75b08f57f2cadde322f92b33eaba6f33
Philippe Paris <contacto@philippeparis.net>
2022-10-18 21:02:58 -0300

    Updates

---
+++ precache-manifest.8de2a041300f38b36aa26b03dc421311.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c5dddea0457491291c4",
+    "url": "/todo-app/static/css/main.61640aeb.chunk.css"
+  },
+  {
+    "revision": "1c5dddea0457491291c4",
+    "url": "/todo-app/static/js/main.1c5dddea.chunk.js"
+  },
+  {
+    "revision": "1c40035b084f5b2093b7",
+    "url": "/todo-app/static/js/1.1c40035b.chunk.js"
+  },
+  {
+    "revision": "84c007b1203ce2fd8a61",
+    "url": "/todo-app/static/js/runtime~main.84c007b1.js"
+  },
+  {
+    "revision": "6e2547483ea62875c9f2cacae6b6254c",
+    "url": "/todo-app/index.html"
+  }
+];
